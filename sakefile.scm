751a814971f84bcf44d3501ff32fc63c84222459
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2012-04-28 21:43:51 +0200

    using Sake for tasks

---
+++ sakefile.scm
@@ -0,0 +1,27 @@
+(##include "~~prelude/prelude#.scm")
+(define lib-directory "lib/")
+(define lib-name "sdl2")
+(define lib-suffix ".o1")
+
+(define-task init ()
+  (make-directory (current-build-directory)))
+
+(define-task clean (init)
+  (delete-file (current-build-directory))
+  (delete-file lib-directory))
+
+(define-task compile (init)
+  (gambit-eval
+    "
+    (begin
+     (include \"~~prelude/prelude#.scm\")
+     (compile-file \"module.scm\" output: \"build/sdl2.o1\" cc-options: \"-w -I/usr/local/include/SDL2\" ld-options: \"-L/usr/local/lib -lSDL2\"))"))
+
+(define-task install (compile)
+  (make-directory lib-directory)
+  (delete-file (string-append lib-directory lib-name lib-suffix))
+  (copy-file (string-append (current-build-directory) lib-name lib-suffix)
+             (string-append lib-directory lib-name lib-suffix)))
+
+(define-task all (compile install)
+  '(compile and install))
