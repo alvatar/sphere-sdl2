1295944bf7f9cf33f92db46e3fba577fb7b746b4
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2012-05-02 02:40:11 +0200

    compile to C

--- sakefile.scm
+++ sakefile.scm
@@ -1,6 +1,7 @@
 (define lib-directory "lib/")
 (define lib-name "sdl2")
 (define lib-suffix ".o1")
+(define c-suffix ".c")
 
 (define-task init ()
   (make-directory (current-build-directory)))
@@ -13,13 +14,27 @@
   (gambit-eval-here
    '(begin
       (include "~~prelude/prelude#.scm")
-      (compile-file "module.scm" output: "build/sdl2.o1" cc-options: "-w -I/usr/local/include/SDL2" ld-options: "-L/usr/local/lib -lSDL2"))))
+      (compile-file "module.scm"
+                    output: "build/sdl2.o1"
+                    cc-options: "-w -I/usr/local/include/SDL2"
+                    ld-options: "-L/usr/local/lib -lSDL2"))))
 
-(define-task install (compile)
+(define-task compile-to-c (init)
+  (gambit-eval-here
+   `(begin
+      (include "~~prelude/prelude#.scm")
+      (compile-file-to-c
+       "module.scm"
+       output: ,(string-append (current-build-directory) lib-name c-suffix)))))
+
+(define-task install (compile compile-to-c)
   (make-directory lib-directory)
   (delete-file (string-append lib-directory lib-name lib-suffix))
+  (delete-file (string-append lib-directory lib-name c-suffix))
   (copy-file (string-append (current-build-directory) lib-name lib-suffix)
-             (string-append lib-directory lib-name lib-suffix)))
+             (string-append lib-directory lib-name lib-suffix))
+  (copy-file (string-append (current-build-directory) lib-name c-suffix)
+             (string-append lib-directory lib-name c-suffix)))
 
 (define-task all (compile install)
   '(compile and install))
