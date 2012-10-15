2f99fa3aadc725a0454b2a010249087be3c7801e
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2012-10-15 21:20:17 +0200

    add dependencies in config.scm

--- config.scm
+++ config.scm
@@ -1,1 +1,12 @@
 (sphere: "sdl2")
+(dependencies:
+ (sdl2
+  (include
+   (base: ffi#))
+  (load
+   (base: ffi)))
+ ((sdl2: sdl2 version: (debug))
+  (include
+   (base: ffi# version: (debug)))
+  (load
+   (base: ffi version: (debug)))))
