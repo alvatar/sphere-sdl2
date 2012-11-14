4f621c7418fbef0c44cfaaf54ba166b4fec3a5d5
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2012-11-14 15:36:07 +0100

    removed header, no private procedures

--- config.scm
+++ config.scm
@@ -2,11 +2,13 @@
 (dependencies:
  (sdl2
   (include
-   (base: ffi#))
+   (base: ffi-macros)
+   (sdl2: sdl2-macros))
   (load
    (base: ffi)))
  ((sdl2: sdl2 version: (debug))
   (include
-   (base: ffi# version: (debug)))
+   (base: ffi-macros version: (debug))
+   (sdl2: sdl2-macros version: (debug)))
   (load
    (base: ffi version: (debug)))))
