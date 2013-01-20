725ea52399074d239a3b54413b09f59348bee783
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2013-01-20 16:53:50 +0100

    not handling debug versions manually any more

--- config.scm
+++ config.scm
@@ -8,10 +8,4 @@
    (core: ffi-header)
    (sdl2: sdl2-header))
   (load
-   (core: ffi)))
- ((sdl2: sdl2 version: (debug))
-  (include
-   (core: ffi-macros version: (debug))
-   (sdl2: sdl2-header version: (debug)))
-  (load
-   (core: ffi version: (debug)))))
+   (core: ffi))))
