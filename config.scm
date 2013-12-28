62211c1aa14ae77e1ecfc4f4b3bb4e3774a0ac3c
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2013-12-28 14:27:08 +0100

    add -w flag

--- config.scm
+++ config.scm
@@ -4,25 +4,25 @@
   (load
    (sdl2: sdl2)))
  (sdl2
-  (cc-options (pkg-config--cflags "sdl2"))
+  (cc-options (pkg-config--cflags "sdl2") "-w")
   (ld-options (pkg-config--libs "sdl2"))
   (prelude
    (core: ffi-header)
    (sdl2: sdl2-header)))
  (sdl2-image
-  (cc-options (pkg-config--cflags "SDL2_image"))
+  (cc-options (pkg-config--cflags "SDL2_image") "-w")
   (ld-options (pkg-config--libs "SDL2_image"))
   (prelude
    (core: ffi-header)
    (sdl2: sdl2-header)))
  (sdl2-mixer
-  (cc-options (pkg-config--cflags "SDL2_mixer"))
+  (cc-options (pkg-config--cflags "SDL2_mixer") "-w")
   (ld-options (pkg-config--libs "SDL2_mixer"))
   (prelude
    (core: ffi-header)
    (sdl2: sdl2-header)))
  (sdl2-ttf
-  (cc-options (pkg-config--cflags "SDL2_ttf"))
+  (cc-options (pkg-config--cflags "SDL2_ttf") "-w")
   (ld-options (pkg-config--libs "SDL2_ttf"))
   (prelude
    (core: ffi-header)
