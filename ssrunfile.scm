e2fad93a98ae679b7b0f2ed882958cc8fa96deeb
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2015-04-13 23:42:14 +0200

    Linux uses case-sensitive library names

--- ssrunfile.scm
+++ ssrunfile.scm
@@ -6,7 +6,7 @@
          lib
          cond-expand-features: '(debug)
          cc-options: "-I../../deps/SDL2-2.0.3/include/ -w"
-         ld-options: "-lsdl2")))
+         ld-options: "-lSDL2")))
     ((spheres/sdl2 sdl2-image)
      .
      ,(lambda (lib)
@@ -14,7 +14,7 @@
          lib
          cond-expand-features: '(debug)
          cc-options: "-I../../deps/SDL2-2.0.3/include/ -I../../deps/SDL2_image-2.0.0 -w"
-         ld-options: "-lsdl2 -lsdl2_image")))
+         ld-options: "-lSDL2 -lSDL2_image")))
     ((spheres/sdl2 sdl2-mixer)
      .
      ,(lambda (lib)
@@ -22,7 +22,7 @@
          lib
          cond-expand-features: '(debug)
          cc-options: "-I../../deps/SDL2-2.0.3/include/ -I../../deps/SDL2_mixer-2.0.0 -w"
-         ld-options: "-lsdl2 -lsdl2_mixer")))
+         ld-options: "-lSDL2 -lSDL2_mixer")))
     ((spheres/sdl2 sdl2-ttf)
      .
      ,(lambda (lib)
@@ -30,7 +30,7 @@
          lib
          cond-expand-features: '(debug)
          cc-options: "-I../../deps/SDL2-2.0.3/include/ -I../../deps/SDL2_ttf-2.0.12 -w"
-         ld-options: "-lsdl2 -lsdl2_ttf")))))
+         ld-options: "-lSDL2 -lSDL2_ttf")))))
 
 (define-task (compile library) ()
   ;; This fixes and issue in OSX: pkg-config not finding GL
