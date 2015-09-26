6b42d77e52606f27b66b00d55671388956d19dcb
ovenpasta <ovenpasta@pizzahack.eu>
2015-09-26 22:22:37 +0200

    added sdl2-config support

--- ssrunfile.scm
+++ ssrunfile.scm
@@ -1,38 +1,53 @@
 (include "~~lib/ssrun/tasks/core.scm")
 
+(define (sdl2-config-cc)
+  (string-append
+   (with-input-from-process
+    (list path: "sdl2-config"
+	  arguments: '("--cflags")) read-line)
+   "-w"))
+
+(define (sdl2-config-ld . static)
+  (with-input-from-process
+   (list path: "sdl2-config"
+	 arguments: (list (if (null? static) 
+			  "--libs" 
+			  "--static-libs")))
+   read-line))
+  
 (define libraries
   `(((spheres/sdl2 sdl2)
      .
      ,(lambda (lib)
-        (ssrun#compile-library
-         lib
-         cond-expand-features: '(debug)
-         cc-options: "-I/usr/include/SDL2/ -w"
-         ld-options: "-lSDL2")))
+	(ssrun#compile-library
+	   lib
+	   cond-expand-features: '(debug)
+	   cc-options: (sdl2-config-cc)
+	   ld-options: (sdl2-config-ld))))
     ((spheres/sdl2 sdl2-image)
      .
      ,(lambda (lib)
         (ssrun#compile-library
          lib
          cond-expand-features: '(debug)
-         cc-options: "-I/usr/include/SDL2/ -w"
-         ld-options: "-lSDL2 -lSDL2_image")))
+         cc-options: (sdl2-config-cc)
+         ld-options: (string-append (sdl2-config-ld) " " "-lSDL2_image"))))
     ((spheres/sdl2 sdl2-mixer)
      .
      ,(lambda (lib)
         (ssrun#compile-library
          lib
          cond-expand-features: '(debug)
-         cc-options: "-I/usr/include/SDL2/ -w"
-         ld-options: "-lSDL2 -lSDL2_mixer")))
+         cc-options: (sdl2-config-cc)
+         ld-options: (string-append (sdl2-config-ld) " " "-lSDL2_mixer"))))
     ((spheres/sdl2 sdl2-ttf)
      .
      ,(lambda (lib)
         (ssrun#compile-library
          lib
          cond-expand-features: '(debug)
-         cc-options: "-I/usr/include/SDL2 -w"
-         ld-options: "-lSDL2 -lSDL2_ttf")))))
+         cc-options: (sdl2-config-cc)
+         ld-options:  (string-append (sdl2-config-ld) " " "-lSDL2_ttf"))))))
 
 (define-task (compile library) ()
   ;; This fixes and issue in OSX: pkg-config not finding GL
