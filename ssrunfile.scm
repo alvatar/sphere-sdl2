7d380ca84ca95d73efc50932c8c5eacdb9439fff
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2015-09-30 15:06:16 +0200

    Check for sdl2-config before compiling

--- ssrunfile.scm
+++ ssrunfile.scm
@@ -1,20 +1,21 @@
 (include "~~lib/ssrun/tasks/core.scm")
+(include "~~lib/ssrun/tasks/minimal.scm")
 
 (define (sdl2-config-cc)
   (string-append
    (with-input-from-process
     (list path: "sdl2-config"
 	  arguments: '("--cflags")) read-line)
-   "-w"))
+   " -Wno-incompatible-pointer-types -Wno-discarded-qualifiers"))
 
 (define (sdl2-config-ld . static)
   (with-input-from-process
    (list path: "sdl2-config"
-	 arguments: (list (if (null? static) 
-			  "--libs" 
+	 arguments: (list (if (null? static)
+			  "--libs"
 			  "--static-libs")))
    read-line))
-  
+
 (define libraries
   `(((spheres/sdl2 sdl2)
      .
@@ -50,6 +51,20 @@
          ld-options:  (string-append (sdl2-config-ld) " " "-lSDL2_ttf"))))))
 
 (define-task (compile library) ()
+  (define (sdl-error . dummy)
+    (println "Please make sure SDL2 is properly installed (version >= 2.0.0) - Sphere SDL2 relies on sdl2-config for compilation")
+    (exit 1))
+  (let* ((version-str
+          (with-exception-handler
+           sdl-error
+           (lambda ()
+             (with-input-from-process '(path: "sdl2-config" arguments: ("--version")) read-line))))
+         (version-list (map string->number ((string-split #\.) version-str))))
+    (unless (and (= (length version-list) 3)
+                 (= (car version-list) 2)
+                 (>= (cadr version-list) 0)
+                 (>= (caddr version-list) 0))
+            (sdl-error)))
   ;; This fixes and issue in OSX: pkg-config not finding GL
   ;; (if (eq? (sake#host-platform) 'osx)
   ;;     (setenv "PKG_CONFIG_PATH" "/opt/X11/lib/pkgconfig/"))
